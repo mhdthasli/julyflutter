91eea151098d94c2d0a7cf84d5374c1d29fbd5d0
USER <mohammedthasli0@gmail.com>
2023-09-08 00:57:50 +0530

    commit

--- lib/sample.dart
+++ lib/sample.dart
@@ -13,12 +13,36 @@ class MyApp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("My app")),
       body: Center(
-        child: Container(
-          color: Colors.deepPurple,
-          child: Text(
-            "welcome to flutter",
-            style: TextStyle(
-                color: Colors.amber, fontSize: 30, fontStyle: FontStyle.italic),
+        child: Container(height: double.infinity,
+          width: double.infinity,
+          color: Colors.white,
+          child:
+          Column(
+            children: [
+              Padding(
+                padding:EdgeInsets.only(top: 100 ,right:95),
+                child: Text(
+                  "WELCOME TO KERALA",
+                  style: TextStyle(
+                      color: Colors.black, fontSize: 50,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+                ),
+              ),
+              Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 300,top: 60),
+                child: Image(image: AssetImage("assets/icons/Iconarchive-Incognito-Animal-2-Dino-Tyrannosaurus.512.png" ),height: 200,width: 300),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(right : 300,top: 60),
+                child: Image(image: AssetImage("assets/icons/Iconarchive-Incognito-Animal-2-Wolf.512.png"),height: 200,width: 300),
+              ),
+
+
+                  ],
+                ),
+            ],
           ),
         ),
       ),
