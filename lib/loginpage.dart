6a05bc2af8b0364b5073e2e6fdaecbab62906b26
USER <mohammedthasli0@gmail.com>
2023-09-12 00:26:06 +0530

    commit

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -12,6 +12,7 @@ class loginpage extends StatefulWidget {
 }
 
 class loginstate extends State<loginpage> {
+  bool showpass=true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,14 +27,13 @@ class loginstate extends State<loginpage> {
               Text(
                 "Login Page",
                 style: TextStyle(
-                    fontStyle: FontStyle.italic,
                     fontWeight: FontWeight.bold,
                     fontSize: 25,
                     color: Colors.black),
               ),
               SizedBox(height: 50),
               Padding(
-                padding: const EdgeInsets.only(right: 400, left: 400),
+                padding: const EdgeInsets.only(right: 100, left: 100),
                 child: TextField(
                   decoration: InputDecoration(
                       labelText: "username",
@@ -45,29 +45,50 @@ class loginstate extends State<loginpage> {
               ),
               SizedBox(height: 40),
               Padding(
-                padding: const EdgeInsets.only(right: 400, left: 400),
+                padding: const EdgeInsets.only(right: 100, left: 100),
                 child: TextField(
+                  obscureText: showpass,
+                  obscuringCharacter: "*",
                   decoration: InputDecoration(
                       labelText: "password",
                       hintText: "password",
                       prefixIcon: Icon(Icons.password),
+                      suffixIcon: IconButton(onPressed: () { setState(() {
+                        if(showpass){
+                        showpass=false;
+                         }
+                        else{
+                          showpass=true;
+                        }
+                      }); }, icon: Icon(showpass==true ? Icons.visibility_off:Icons.visibility),),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(30))),
                 ),
               ),
-              SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  "LOGIN",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.bold),
+              SizedBox(height: 50),
+              TextButton(onPressed: (){}, child:Text("Forget Password")),
+              SizedBox(height: 30),
+              SizedBox(
+                height: 40,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "LOGIN",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+
+                  ),
                 ),
-              )
+              ),
+              SizedBox(height: 30),
+              TextButton(onPressed: (){}, child:Text("SignUp")),
+              SizedBox(height: 0,),
+              TextButton(onPressed: (){}, child: Text("Not a User?  Create a Account")),
             ],
           ),
+
         ),
       ),
     );
--- lib/sample.dart
+++ lib/sample.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:julyflutter/main.dart';
+
+import 'loginpage.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -7,41 +11,55 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => loginpage()));
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("My app")),
       body: Center(
-        child: Container(height: double.infinity,
+        child: Container(
+          height: double.infinity,
           width: double.infinity,
           color: Colors.white,
-          child:
-          Column(
+          child: Column(
             children: [
               Padding(
-                padding:EdgeInsets.only(top: 100 ,right:95),
+                padding: EdgeInsets.only(top: 100),
                 child: Text(
                   "WELCOME TO KERALA",
                   style: TextStyle(
-                      color: Colors.black, fontSize: 50,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+                      color: Colors.black,
+                      fontSize: 50,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
               Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 300,top: 60),
-                child: Image(image: AssetImage("assets/icons/Iconarchive-Incognito-Animal-2-Dino-Tyrannosaurus.512.png" ),height: 200,width: 300),
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 200,right: 100),
+                    child: Image(
+                        image: AssetImage(
+                            "assets/icons/Iconarchive-Incognito-Animal-2-Dino-Tyrannosaurus.512.png"),
+                        height: 200,
+                        width: 300),
+                  ),
+                ],
               ),
-
-              Padding(
-                padding: const EdgeInsets.only(right : 300,top: 60),
-                child: Image(image: AssetImage("assets/icons/Iconarchive-Incognito-Animal-2-Wolf.512.png"),height: 200,width: 300),
-              ),
-
-
-                  ],
-                ),
             ],
           ),
         ),
