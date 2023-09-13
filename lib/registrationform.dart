61664f8925451319420ba804c05cd6f9c23fef36
USER <mohammedthasli0@gmail.com>
2023-09-13 19:50:09 +0530

    commit

--- lib/registrationform.dart
+++ lib/registrationform.dart
@@ -3,161 +3,169 @@ import 'dart:html';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(home: Registrationform(),));
-
+void main() {
+  runApp(MaterialApp(
+    home: Registrationform(),
+  ));
 }
-class Registrationform extends StatefulWidget{
+
+class Registrationform extends StatefulWidget {
   @override
-  State<StatefulWidget> createState()=>RegistrationState();
+  State<StatefulWidget> createState() => RegistrationState();
+}
 
+class RegistrationState extends State<Registrationform> {
+  bool showpass = true;
 
-
-  }
-
-class RegistrationState extends State<Registrationform>{
-  bool showpass=true;
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text("RegistrationForm"),
-
-    ),
-
-    body: Container(
-      child: Column(
-        children: [
-          SizedBox(height: 5),
-          Text(
-              "RegistrationForm",style: TextStyle(fontSize:30,fontWeight: FontWeight.bold,color: Colors.black87),),
-          SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.only(right: 30,left: 30),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "FrstName",
-                hintText: "FistName",
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(40),
-
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.only(right: 30,left: 30),
-            child: TextField(
-              decoration:InputDecoration(
-                labelText: "LastName",
-                hintText: "LastName",
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(40),
-                ),
-              ) ,
-
-            ),
-          ),
-          SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.only(right: 30,left: 30),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "UserName",
-                hintText: "UserName",
-                prefixIcon: Icon(Icons.login),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(40),
-
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.only(right: 30,left: 30),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "E-mail",
-                hintText: "E-mail",
-                prefixIcon: Icon(Icons.email),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(40),
-
-                ),
-
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.only(right: 30,left: 30),
-            child: TextField(
-              obscureText: showpass,
-              obscuringCharacter: "*",
-              decoration: InputDecoration(
-                  labelText: "password",
-                  hintText: "password",
-                  prefixIcon: Icon(Icons.password),
-                  suffixIcon: IconButton(onPressed: () { setState(() {
-                    if(showpass){
-                      showpass=false;
-                    }
-                    else{
-                      showpass=true;
-                    }
-                  }); }, icon: Icon(showpass==true ? Icons.visibility_off:Icons.visibility),),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40))),
-            ),
-          ),
-          SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.only(right: 30,left: 30),
-              child: TextField(
-              obscureText: showpass,
-              obscuringCharacter: "*",
-              decoration: InputDecoration(
-                  labelText: "Confirm Your Password",
-                  hintText: " Confrim Your Password",
-                  prefixIcon: Icon(Icons.password),
-                  suffixIcon: IconButton(onPressed: () { setState(() {
-                    if(showpass){
-                      showpass=false;
-                    }
-                    else{
-                      showpass=true;
-                    }
-                  }); }, icon: Icon(showpass==true ? Icons.visibility_off:Icons.visibility),),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40))),
-          ),
-            ),
-          SizedBox(height: 10),
-       ElevatedButton(onPressed: (){}, child: Text(
-         "REGISTRATION",
-         style: TextStyle(
-           fontWeight: FontWeight.bold,color: Colors.black87,fontSize: 15,
-           ),
-         ),
-       ),
-
-
-
-        ],
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("RegistrationForm"),
       ),
-    ),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(height: 3),
+            Text(
+              "RegistrationForm",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87),
+            ),
+            SizedBox(height: 3,),
+            TextButton(onPressed: (){}, child: Text("Please Fill In This Form To Create An Account!")),
+            SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.only(right: 30, left: 30),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "FrstName",
+                  hintText: "FistName",
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(right: 30, left: 30),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "LastName",
+                  hintText: "LastName",
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(right: 30, left: 30),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "UserName",
+                  hintText: "UserName",
+                  prefixIcon: Icon(Icons.login),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(right: 30, left: 30),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "E-mail",
+                  hintText: "E-mail",
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(right: 30, left: 30),
+              child: TextField(
+                obscureText: showpass,
+                obscuringCharacter: "*",
+                decoration: InputDecoration(
+                    labelText: "password",
+                    hintText: "password",
+                    prefixIcon: Icon(Icons.password),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (showpass) {
+                            showpass = false;
+                          } else {
+                            showpass = true;
+                          }
+                        });
+                      },
+                      icon: Icon(showpass == true
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40))),
+              ),
+            ),
+            SizedBox(height:10),
+            Padding(
+              padding: const EdgeInsets.only(right: 30, left: 30),
+              child: TextField(
+                obscureText: showpass,
+                obscuringCharacter: "*",
+                decoration: InputDecoration(
+                    labelText: "Confirm Your Password",
+                    hintText: " Confrim Your Password",
+                    prefixIcon: Icon(Icons.password),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (showpass) {
+                            showpass = false;
+                          } else {
+                            showpass = true;
+                          }
+                        });
+                      },
+                      icon: Icon(showpass == true
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40))),
+              ),
+            ),
+            SizedBox(height:6),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "REGISTRATION",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+            SizedBox(height: 1),
+            TextButton(onPressed: (){}, child: Text("Aleady have an account? Login here")),
 
-
-  );
+          ],
+        ),
+      ),
+    );
   }
-
 }
-
-
-
-
